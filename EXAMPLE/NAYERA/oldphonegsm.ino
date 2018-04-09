e99c366ac248987f8aa3f4204a194dc60360380a
Naiera <36535257+Naiera@users.noreply.github.com>
2018-04-09 12:38:00 +0200

    Add files via upload

---
+++ EXAMPLE/NAYERA/oldphonegsm.ino
@@ -0,0 +1,20 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(7, OUTPUT);
+}
+
+void loop() {
+  int sensorValue = analogRead(A0);
+  if (sensorValue > 50)
+{
+  digitalWrite(7, HIGH);
+  Serial.println("Rumble on");
+delay(1000);
+}
+else
+{
+  digitalWrite(7, LOW);
+  Serial.println("Rumble off");
+delay(100);
+}
+}
